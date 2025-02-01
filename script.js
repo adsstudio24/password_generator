e37b51b788185d1767c2f489501cc60afe2faead
adsstudio24 <132486128+adsstudio24@users.noreply.github.com>
2025-02-01 19:57:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    document.getElementById("password").value = password;
+}
